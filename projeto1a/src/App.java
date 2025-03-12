22802025804ddf6a3135831f1f8938f6fd4dc2fd
Matheus Masera <matheusmasera11@gmail.com>
2025-03-12 00:51:35 +0000

    correcao bugs

--- projeto1a/src/App.java
+++ projeto1a/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //bug resolvido
         System.out.println("\nHello!");
         String version = System.getProperty("java.version");
         System.out.println("Running java version "+version+"\n");
