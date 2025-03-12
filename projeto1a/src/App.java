7441b896385adf29892a425068313f729dc2d271
Matheus Masera <matheusmasera11@gmail.com>
2025-03-12 00:24:44 +0000

    adicionando mudancas

--- projeto1a/src/App.java
+++ projeto1a/src/App.java
@@ -3,6 +3,6 @@ public class App {
         System.out.println("\nHello!");
         String version = System.getProperty("java.version");
         System.out.println("Running java version "+version+"\n");
-        System.out.println("Testando mudanças" + "\n");
+        System.out.println("Testando algumas mudanças" + "\n");
     }
 }
